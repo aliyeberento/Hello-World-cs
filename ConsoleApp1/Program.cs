3d5c994532816104af67587db639763cc96ce4ff
Aliye Berento <berento.aliye@gmail.com>
2022-06-12 02:44:53 -0500

    Created a class to store my personal information and displayed it in the console.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,26 @@
 
 namespace helloWorld
 {
+
+   public class personalInfo
+    {
+        public string firstName = "";
+        public string lastName = "";
+        public int age ;
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            personalInfo personalInfo = new personalInfo();
+            personalInfo.firstName = "Aliye";
+            personalInfo.lastName = "Berento";
+            personalInfo.age = 21;
+            Console.WriteLine("My name is " + personalInfo.firstName +" " + personalInfo.lastName + " and I'm " + personalInfo.age);
         }
     }
-}
+}  
